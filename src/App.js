a27a4f50c88dbc234bd0d6e72c823c1e01f01fb5
Kikaaa32 <kristina.jurac20@gmail.com>
2024-05-28 20:26:32 +0200

    nova userdetails komponenta

--- src/App.js
+++ src/App.js
@@ -1,41 +1,6 @@
 import React, { useState, useEffect } from 'react';
-//import UserDetails from './UserDetails';
+import UserDetails from './UserDetails';
 import "./App.css";
-
-function UserDetails({ userData }) {
-  const [repos, setRepos] = useState([]);
-
-  useEffect(() => {
-    fetchUserRepos();
-  }, [userData]);
-
-  const fetchUserRepos = async () => {
-    try {
-      const response = await fetch(userData.repos_url);
-      const data = await response.json();
-      setRepos(data);
-    } catch (error) {
-      console.error('Error fetching user repos:', error);
-    }
-  };
-
-  return (
-    <div className='details'>
-      <div className='userImage'>
-        <img src={userData.avatar_url} alt="User Avatar" /> 
-        <label><b>{userData.name}</b></label>
-      </div>
-      <p><b>BIO:</b> {userData.bio}</p>
-      <p><b>LOCATION:</b> {userData.location}</p>
-      <p><b>REPOSITORIES:</b></p>
-      <ul>
-        {repos.map((repo) => (
-          <li key={repo.id}>{repo.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
 
 function App() {
   const [username, setUsername] = useState('');
